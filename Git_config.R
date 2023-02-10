8619fca9286ee3898dee52fd783285d8d72d72f0
CharlesDexterW <agarces2381@gmail.com>
2023-02-09 23:42:17 -0500

    Test 1
    Plot works
    
    Exercise 1
    
    Plot successfull

---
+++ Git_config.R
@@ -0,0 +1,15 @@
+## set your user name and email:
+usethis::use_git_config(user.name = "YourName", user.email = "your@mail.com")
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## set personal access token:
+credentials::set_github_pat("YourPAT")
+
+## or store it manually in '.Renviron':
+usethis::edit_r_environ()
+## store your personal access token in the file that opens in your editor with:
+## GITHUB_PAT=xxxyyyzzz
+## and make sure '.Renviron' ends with a newline
